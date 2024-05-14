5fd3ca50827c524532f2c022b74e32189189418f
NoahTheBuzz <noah.sarcevic@gmail.com>
2024-05-14 14:09:22 +0200

    added

---
+++ divide-and-collect/src/main/java/com/divncol/nrn/DNCServer.java
@@ -0,0 +1,56 @@
+package com.divncol.nrn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class DNCServer {
+    
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(1407);
+            ExecutorService service = Executors.newCachedThreadPool();
+
+            while(true) {
+                Socket connection = server.accept();
+                service.submit(new DNCHandler(connection));
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class DNCHandler implements Runnable {
+    private final Socket connection;
+    private final BufferedReader reader;
+    private final PrintWriter writer;
+
+    private final LinkedBlockingQueue<> queue;
+
+    private String username;
+    private String password;
+
+    public DNCHandler(Socket connection) {
+        this.connection = connection;
+        this.reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        this.writer = new PrintWriter(connection.getOutputStream());
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            connection.getOutputStream().write("Hello to DivNCol".getBytes());
+            connection.getOutputStream().write("LogIn or SignUp? (L/S)".getBytes());
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
